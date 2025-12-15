c97da64549866f860f04933a898a0955ce2a9660
TDat2005 <vdsvcxd@gmail.com>
2025-12-15 13:04:12 +0700

    36

--- BT/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Kết nối database
-$conn = mysqli_connect("localhost", "root", "", "74dctt25_qltv");
-
-if (!$conn) {
-    die("Kết nối thất bại: " . mysqli_connect_error());
-}
--- BT/ttt.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo 'PHP version: ' . phpversion() . "<br>";
-echo 'ZipArchive: ' . (class_exists('ZipArchive') ? 'YES' : 'NO');
-?>
